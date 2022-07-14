b7c786bbeaf8209b8c8b47e20bf66fd42db4b522
kohlia <kohlia@mskcc.org>
2022-07-14 01:28:16 -0400

    MAINT: testing pre-commit

--- gsxform/graph.py
+++ gsxform/graph.py
@@ -1,7 +1,6 @@
-"""graph utility functions
+"""graph utility functions.
 
-TODO:
-
+Note: diag(L) == W.sum(1)
 """
 
 from typing import Tuple
@@ -10,41 +9,39 @@ import torch
 
 
 def adjacency_to_laplacian(W: torch.Tensor) -> torch.Tensor:
-    """convert adjacency matrix into the graph Laplacian
+    """Convert adjacency matrix into the graph Laplacian.
 
     Parameters
     ----------
     W: torch.Tensor
-        Batch of normalized graph adjacency matricies
+        Batch of normalized graph adjacency matricies.
 
     Returns
     -------
     torch.Tensor
-        Batch of graph Laplacians
+        Batch of graph Laplacians.
 
     """
-
     L = torch.diag_embed(W.sum(1)) - W
 
     return L
 
 
 def normalize_adjacency(W: torch.Tensor) -> torch.Tensor:
-    """normalize adjacency matrix
+    """Normalize the adjacency matrix.
 
     Parameters
     ----------
     W: torch.Tensor
-        Batch of adjacency matricies
+        Batch of adjacency matricies.
 
     Returns
     -------
     torch.Tensor
-        Batch of normalized adjacency matricies
+        Batch of normalized adjacency matricies.
 
     """
-
-    # build degree vector (diag(L) is the same as W.sum(1)
+    # build degree vector
     d = W.sum(1)
     # normalize
     D_invsqrt = torch.diag_embed(1.0 / torch.sqrt(torch.max(torch.ones(d.size()), d)))
@@ -54,20 +51,19 @@ def normalize_adjacency(W: torch.Tensor) -> torch.Tensor:
 
 
 def normalize_laplacian(L: torch.Tensor) -> torch.Tensor:
-    """normalize graph Laplacian
+    """Normalize the graph Laplacian.
 
     Parameters
     ----------
     L: torch.Tensor
-        Batch of graph Laplacians
+        Batch of graph Laplacians.
 
     Returns
     -------
     torch.Tensor
-        Batch of normalized graph Laplacians
+        Batch of normalized graph Laplacians.
     """
-
-    # build degree vector (diag(L) is the same as W.sum(1)
+    # build degree vector
     d = torch.diag(L)
     # normalize
     D_invsqrt = torch.diag_embed(1.0 / torch.sqrt(torch.max(torch.ones(d.size()), d)))
@@ -77,19 +73,18 @@ def normalize_laplacian(L: torch.Tensor) -> torch.Tensor:
 
 
 def compute_spectra(W: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-    """computes spectra of graph laplacian from its adjacency matrix
+    """Compute spectra of graph Laplacian from its adjacency matrix.
 
     Parameters
     ----------
     W: torch.Tensor
-       Batch of graph adjacency matricies
+       Batch of graph adjacency matricies.
 
     Returns
     -------
     Tuple[torch.Tensor, torch.Tensor]
-        Batch of eigenvalues and eigenvectors of the graph laplacian
+        Batch of eigenvalues and eigenvectors of the graph Laplacian.
     """
-
     # compute laplacian
     L = adjacency_to_laplacian(W)
     # normalize laplacian
