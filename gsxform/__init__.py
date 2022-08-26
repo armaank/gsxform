8a3497fa314c61ef4e11fbf0b392db14c9f66886
kohlia <kohlia@mskcc.org>
2022-08-26 18:57:44 -0400

    DOCS: updating readme, adjutments to init

--- gsxform/__init__.py
+++ gsxform/__init__.py
@@ -1,12 +1,12 @@
 """gsxform."""
 
-# from .graph import (
-#    adjacency_to_laplacian,
-#    normalize_adjacency,
-#    normalize_laplacian,
-#    compute_spectra,
-# )
-# from .kernel import hann_kernel, spline_kernel
+from .graph import (
+    adjacency_to_laplacian,
+    compute_spectra,
+    normalize_adjacency,
+    normalize_laplacian,
+)
+from .kernel import hann_kernel, spline_kernel
 from .scattering import Diffusion, Spline, TightHann
 from .wavelets import diffusion_wavelets, hann_wavelets, spline_wavelets
 
@@ -17,4 +17,10 @@ __all__ = [
     "Diffusion",
     "Spline",
     "TightHann",
+    "hann_kernel",
+    "spline_kernel",
+    "adjacency_to_laplacian",
+    "normalize_adjacency",
+    "normalize_laplacian",
+    "compute_spectra",
 ]
